c2af3b6e237ea44181cc830ff076e412217b5d13
Heeyeon KIM <yyyy1221@naver.com>
2019-11-06 21:34:27 +0900

    name.c파일 추가

---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+	char echo_string[50] = "echo";
+	char name[30];
+	scanf("%s", name);
+	strcat(echo_string, name);
+	strcat(echo_string, ">");
+	strcat(echo_string, name);
+	system(echo_string);
+}
