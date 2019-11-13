7621fd9cceae4685475076aff29f869012ae26f5
Prid <w8385@naver.com>
2019-11-13 22:31:24 +0900

    name.txt와 연동

--- make_chatting_list.c
+++ make_chatting_list.c
@@ -11,26 +11,30 @@ int main() {
 }
 
 void make_chatting_room() {
-
-	int check;
+	FILE *name_fp;
 	char echo_string[50] = "echo ";
-	char name[30] = "name"; 
+	char name[30]; 
 	char chatting_partner[30];
+
 	printf("대화를 원하는 상대방의 Git 아이디를 입력해주세요 : ");
 	scanf("%s",chatting_partner);
+	
+	name_fp = fopen("./name.txt","rt");
+	fscanf(name_fp, "%s", name);
 
-	check = strcmp(chatting_partner, name);
-	if (check == 1) {
+	if (strcmp(chatting_partner, name) < 0) { //partner_name
 		strcat(echo_string, ">");
 		strcat(echo_string, chatting_partner);
 		strcat(echo_string, "_");
 		strcat(echo_string, name);
 	}
-	else{
+	else if(strcmp(chatting_partner, name) > 0) { //name_partner
 		strcat(echo_string, ">");
 		strcat(echo_string, name);
 		strcat(echo_string, "_");
 		strcat(echo_string, chatting_partner);
+	}else{
+		printf("자신과의 대화");
 	}
 	system(echo_string);
 }
