e6342dd5c7572802235b4b7ce521e10a4ccbafb0
최시언 <54929601+noino0819@users.noreply.github.com>
2019-11-20 19:09:09 +0900

    URL등록과 push함수 등록

--- make_chatting_list.c
+++ make_chatting_list.c
@@ -2,22 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-void make_chatting_room();
+void check_where_make_room(); //채팅방을 만드는 위치 결정
+void make_chatting_room(); //채팅방 만들기
+void push_chatting_room(); //채팅방을 GIT에 업로드
 
 int main() {
+	check_where_make_room();
 	make_chatting_room();
+	push_chatting_room();
+}
+
+void check_where_make_room() {
+	printf("채팅방을 업로드할 깃허브 주소를 알려주세요");
+	char Git_address[30];
+	scanf("%s", Git_address);
+	char echo_string[60] = "git remote add GitTalk ";
+	strcat(echo_string, Git_address);
+	system(echo_string);
 }
 
 void make_chatting_room() {
-	FILE *name_fp;
-	char echo_string[50] = "echo Chatting/"
+	FILE* name_fp;
+	char echo_string[50] = "echo Chatting/";
 	char name[30];
 	char chatting_partner[30];
 
 	printf("대화를 원하는 상대방의 Git 아이디를 입력해주세요 : ");
-	scanf("%s",chatting_partner);
-	
-	name_fp = fopen("./name.txt","rt");
+	scanf("%s", chatting_partner);
+
+	name_fp = fopen("./name.txt", "rt");
 	fscanf(name_fp, "%s", name);
 
 	if (strcmp(chatting_partner, name) < 0) { //partner_name
@@ -26,14 +39,20 @@ void make_chatting_room() {
 		strcat(echo_string, "_");
 		strcat(echo_string, name);
 	}
-	else if(strcmp(chatting_partner, name) > 0) { //name_partner
+	else if (strcmp(chatting_partner, name) > 0) { //name_partner
 		strcat(echo_string, ">");
 		strcat(echo_string, name);
 		strcat(echo_string, "_");
 		strcat(echo_string, chatting_partner);
-	}else{
+	}
+	else {
 		printf("자신과의 대화");
 	}
 	printf("%s", echo_string);
 	system(echo_string);
+}
+
+void push_chatting_room(void) {
+	char push_room[30] = "git push GitTalk master"; //지금은 마스터로 사용하지만, 이후에 Git브랜치 이름을 정할 경우 수정
+	system(push_room);
 }
