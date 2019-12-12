ac635e22f7012a7fcc66d82408eefc96382d5548
mingeun0507 <tqdlfkg@naver.com>
2019-12-12 03:00:57 +0000

    merge conflict 해결 방법 구현

--- GitTalk.c
+++ GitTalk.c
@@ -672,27 +672,32 @@ void chatting(char *chatting_file){
 			fprintf(ofp, "%s\n", total_msg);
 			fclose(ofp);
 			system(add_string);
-			system("git commit -m 'chatting_test_commit' > /dev/null 2> /dev/null"); //나중에 커밋 메시지 수정 예정
 			printf("git commit 실행 중...\n");
-			system("git pull > /dev/null 2> /dev/null");
+			system("git commit -m 'chatting_test_commit' > /dev/null 2> /dev/null"); //나중에 커밋 메시지 수정 예정
 			printf("git pull 실행 중...\n");
-			system(push_string);
+			system("git pull > /dev/null 2> /dev/null");
 			printf("git push 실행 중...\n");
+			system(push_string);
 			ifp = fopen("push_err_log.txt", "rt");
 			fscanf(ifp, "%[^\n]\n", buf); //push_err_log 파일의 첫 줄
 			fscanf(ifp, "%[^\n]\n", buf); //push_err_log 파일의 두번째 줄
 			fclose(ifp);
 			if (buf[0] == '!'){ //push 오류 발생 (다시 pull이 필요한 경우)
 				printf("git push 오류 발생!\n");
-				system("git pull > pull_log.txt 2> /dev/null");
 				printf("git pull 재시도 중...\n");
+				system("git pull > pull_log.txt 2> /dev/null");
 				ifp = fopen("pull_log.txt", "rt");
 				fscanf(ifp, "%[^\n]\n", buf); //pull_log 파일의 첫 줄
 				fscanf(ifp, "%[^\n]\n", buf); //pull_log 파일의 두번째 줄
 				fclose(ifp);
 				if (buf[0] == 'C'){ //merge conflict 발생 (conflict 제거 후 다시 commit & push 필요)
 					printf("merge conflict 발생!\n");
-					/* merge conflict를 해결하기 위해 |로 채팅 구분하도록 구현, 그리고 merge conflict 해결하는 것 구현 필요*/
+					printf("git merge --abort 실행 중...\n");
+					system("git merge --abort");
+					printf("git reset --hard HEAD~ 실행 중...\n");
+					system("git reset --hard HEAD~");
+					printf("git pull 재실행 중\n");
+					system("git pull > pull_log.txt 2> /dev/null");
 				}
 				system(push_string); //merge conflict 혹은 push 오류 해결 후 다시 push!
 			}
